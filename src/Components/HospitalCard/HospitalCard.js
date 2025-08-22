49cae783d9119642677c932f4043b9eea3f403d9
Archana MohanDas <archanamdasofficial@gmail.com>
2025-08-22 21:16:18 +0530

    component

--- src/Components/HospitalCard/HospitalCard.js
+++ src/Components/HospitalCard/HospitalCard.js
@@ -29,7 +29,7 @@ export default function HospitalCard({
         />
         <Box flex={1}>
           <Typography
-            variant="h1"
+            component="h3"
             color="#2AA7FF"
             fontWeight={600}
             fontSize={{ xs: 18, md: 20 }}
